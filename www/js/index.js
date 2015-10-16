fffa6a7ce7758da62153be548a7ace13089fe1ea
Julius Garcia <garciajuliusb@yahoo.com>
2015-10-16 18:05:34 +0800

    1666

--- www/js/index.js
+++ www/js/index.js
@@ -27,27 +27,46 @@
 
  $(document).on("pageinit", "#index", function () {
         
-        var centerLocation = new google.maps.LatLng('57.77828', '14.17200');
+        // var centerLocation = new google.maps.LatLng('57.77828', '14.17200');
 
-        var myOptions = {
-            center: centerLocation,
-            zoom: 8,
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            callback: function () { alert('callback'); }
-        };
+        // var myOptions = {
+        //     center: centerLocation,
+        //     zoom: 8,
+        //     mapTypeId: google.maps.MapTypeId.ROADMAP,
+        //     callback: function () { alert('callback'); }
+        // };
 
-        var map_element = document.getElementById("map_canvas");
-
-
+        // var map_element = document.getElementById("map_canvas");
 
 
-        map = new google.maps.Map(map_element, myOptions);
 
-        var mapwidth = $(window).width();
-        var mapheight = $(window).height();
-        $("#map_canvas").height(mapheight);
-        $("#map_canvas").width(mapwidth);
-        google.maps.event.trigger(map, 'resize');
+
+        // map = new google.maps.Map(map_element, myOptions);
+
+        // var mapwidth = $(window).width();
+        // var mapheight = $(window).height();
+        // $("#map_canvas").height(mapheight);
+        // $("#map_canvas").width(mapwidth);
+        // google.maps.event.trigger(map, 'resize');
+
+
+		$('#map_canvas').gmap().bind('init', function(evt, map) {
+			$('#map_canvas').gmap('getCurrentPosition', function(position, status) {
+				if ( status === 'OK' ) {
+					var clientPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+					$('#map_canvas').gmap('addMarker', {'position': clientPosition, 'bounds': true});
+					$('#map_canvas').gmap('addShape', 'Circle', { 
+						'strokeWeight': 0, 
+						'fillColor': "#008595", 
+						'fillOpacity': 0.25, 
+						'center': clientPosition, 
+						'radius': 15, 
+						'clickable': false 
+					});
+				}
+			});   
+		});
+
 
 });
 
